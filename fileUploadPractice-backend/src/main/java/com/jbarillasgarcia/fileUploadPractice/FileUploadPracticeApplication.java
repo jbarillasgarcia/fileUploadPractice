491bc2b774c1a71659466c95fd5fbb17b2486e51
Josue Barillas <jbarillasgarcia@gmail.com>
2019-07-22 10:23:28 -0600

    Initial version of file upload practice backend module.

---
+++ fileUploadPractice-backend/src/main/java/com/jbarillasgarcia/fileUploadPractice/FileUploadPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jbarillasgarcia.fileUploadPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadPracticeApplication.class, args);
+	}
+
+}
